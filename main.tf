24ce5e19e3db80702616b9c4d40e6dc4641bfd7f
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-04-03 20:46:53 +0530

    terraform basics

---
+++ main.tf
@@ -0,0 +1,106 @@
+#Learning HCL 
+
+/*this
+is
+a multiline 
+comment */
+
+#================================================================================================#
+
+#Block
+# block_type {
+#     attribute1 = value1
+#     attribute2 = value2
+# }
+
+# #attributes
+# #key-value
+
+# key = value 
+
+#================================================================================================#
+
+#Terraform code to create an instance
+
+# resource "aws_instance" "example" {
+#   ami = "ami-0c94855ba95c71c99"
+#   instance_type = "t2-micro"
+#   count = 2
+  
+# }
+
+
+#================================================================================================#
+
+//Datatypes
+# string
+# number
+# boolean
+
+#================================================================================================#
+
+//List
+
+# list=["item","item1","item2"]
+# security_groups = ["sg-2132","sg-12123"]
+
+#================================================================================================#
+
+//maps
+
+# variable "example_map" {
+#   type = map 
+#   default = {key1 = "value1", key2 = "value2", key3 = "value3"}
+# }
+
+# locals {
+#   my_map = {
+#     "name" = "John Doe",
+#     "age" = "30"
+#     "is_admin" = true
+#   }
+# }
+
+# locals.my_map["name"]
+
+
+#================================================================================================#
+
+//Conditions
+
+# resource "aws_instance" "server" {
+#   instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+
+# }
+
+#================================================================================================#
+
+//Functions
+
+# locals {
+#   name = "John Cena"
+#   fruits = ["Apple","Orange","Banana"]
+#   message = "Hello ${upper(local.name)} ! I know you like ${join("," , local.fruits)}"
+# }
+
+#================================================================================================#
+
+//Resource dependency- used to create relationship b/w 2 diff resource. 2 types are there
+// 1. implicit dependency- which is done automatically by terraform 
+//2. explicit dependency
+
+// eg:   
+
+resource "aws_instance" "name" {
+  vpc_security_group_ids = aws_security_group.mysg.id
+  
+}
+
+resource "aws_security_group" "mysg" {
+   #inbound rules
+}
+
+
+
+
+#================================================================================================#
